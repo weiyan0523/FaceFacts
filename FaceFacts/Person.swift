0b66b1b12c75ec09081fd89d6c40d1f8e82b1879
Wei Yan <weiyan0523@gmail.com>
2024-05-14 22:55:01 -0500

    Add SwiftData and data model

--- FaceFacts/Person.swift
+++ FaceFacts/Person.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Person {
+    
+    var name: String
+    var emailAddress: String
+    var details: String
+    
+    init(name: String, emailAddress: String, details: String) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.details = details
+    }
+}
