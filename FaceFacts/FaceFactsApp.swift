e3acffd6a1b1104775064ea0d27c8c0f31d7fd37
Wei Yan <weiyan0523@gmail.com>
2024-05-14 22:35:11 -0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Wei Yan on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
